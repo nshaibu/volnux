7886a3c928c4904cc4bafc76a82a3a20825636df
Anthony Abeo <anthonyabeo@gmail.com>
2026-01-19 09:22:42 +0000

    add visit method for unary operators in visitor

--- volnux/parser/visitor.py
+++ volnux/parser/visitor.py
@@ -5,6 +5,7 @@ if typing.TYPE_CHECKING:
     from .ast import (
         AssignmentNode,
         BinOpNode,
+        UnaryOpNode,
         BlockNode,
         ConditionalNode,
         DescriptorNode,
@@ -40,6 +41,10 @@ class ASTVisitorInterface(ABC):
 
     @abstractmethod
     def visit_binop(self, node: "BinOpNode"):
+        pass
+
+    @abstractmethod
+    def visit_unaryop(self, node: "UnaryOpNode"):
         pass
 
     @abstractmethod
